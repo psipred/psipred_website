49856095fd49ae51d2569d9aaf80603f50b45404
Daniel Buchan <clone@myself.com>
2015-11-24 10:34:15 -0500

    moved settings stuff

---
+++ psipred_website/settings/dev.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+from unipath import Path
+
+from .base import *
+
+DEV_SECRETS_PATH = SETTINGS_PATH.child("dev_secrets.json")
+with open(os.path.join(DEV_SECRETS_PATH)) as f: secrets = json.loads(f.read())
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'psipred_website_db',
+        'USER': get_secret("USER", secrets),
+        'PASSWORD': get_secret("PASSWORD", secrets),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+SECRET_KEY = get_secret("SECRET_KEY", secrets)
+
+DEBUG = True
+
+INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
