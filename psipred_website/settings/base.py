aa0104ec7bba8a84c16c9ee0125067f6b059817c
Daniel Buchan <clone@myself.com>
2015-11-24 10:54:54 -0500

    skelton on app now in place with requirements and settings configured

--- psipred_website/settings/base.py
+++ psipred_website/settings/base.py
@@ -12,31 +12,50 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import json
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from unipath import Path
+
+from django.contrib import messages
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_secret(setting, secrets):
+    """Get the secret variable or return explicit exception."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+BASE_DIR = Path(__file__).ancestor(3)
+TEMPLATE_PATH = BASE_DIR.child("templates")
+STATIC_PATH = BASE_DIR.child("static")
+STATIC_ROOT = BASE_DIR.child("static_root")
+SETTINGS_PATH = Path(__file__).ancestor(1)
+
+BASE_SECRETS_PATH = SETTINGS_PATH.child("base_secrets.json")
+with open(os.path.join(BASE_SECRETS_PATH)) as \
+    f: base_secrets = json.loads(f.read())
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+8_443r=d%bs5&e#vxnfcahl%%$ik=4#j*1zeoc#2oo63xq)+7'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
 INSTALLED_APPS = (
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
