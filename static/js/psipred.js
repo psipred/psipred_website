d3d748d0f811568909e2e580e239dc339fa002d9
Daniel Buchan <clone@myself.com>
2016-03-01 16:18:52 +0000

    fixed formatting and some variable scoping

--- static/js/psipred.js
+++ static/js/psipred.js
@@ -6,14 +6,15 @@
      6. Show processing animation
      7. listen for result
 */
-var endpoints_url = 'http://127.0.0.1:8000/analytics_automated/endpoints/'
-var submit_url = 'http://127.0.0.1:8000/analytics_automated/submission/'
+var endpoints_url = 'http://127.0.0.1:8000/analytics_automated/endpoints/';
+var submit_url = 'http://127.0.0.1:8000/analytics_automated/submission/';
 
+var bio_d3_data = null;
 
 var ractive = new Ractive({
   el: '#psipred_site',
   template: '#form_template',
-  data: { sequence: '',
+  data: {
           visible: 1,
           psipred_checked: true,
           //psipred_checked: false,
@@ -28,8 +29,8 @@ var ractive = new Ractive({
 
 
 ractive.observe('sequence', function(newValue, oldValue ) {
-  regex = /^>(.+?)\s/
-  match = regex.exec(newValue)
+  regex = /^>(.+?)\s/;
+  match = regex.exec(newValue);
   if(match)
   {
     this.set('name', match[1]);
@@ -41,15 +42,15 @@ ractive.observe('sequence', function(newValue, oldValue ) {
   },
   {init: false,
    defer: true
- })
+ });
 
 ractive.once('poll_trigger', function(){
-   var regex = /\.horiz$/
+   var regex = /\.horiz$/;
 
    var interval = setInterval(function(){
-      url = 'http://127.0.0.1:8000/analytics_automated/submission/'+ractive.get('uuid')
-      var response = send_request(url, "GET", {})
-      data = JSON.parse(response)
+      url = 'http://127.0.0.1:8000/analytics_automated/submission/'+ractive.get('uuid');
+      var response = send_request(url, "GET", {});
+      data = JSON.parse(response);
       for(var k in data){
         if(k == "state"){
           if(data[k] == "Running" || data[k] == "Submitted")
@@ -57,21 +58,22 @@ ractive.once('poll_trigger', function(){
           }
           if(data[k] == 'Complete')
           {
-            results = data['results']
+            results = data['results'];
             for( var i in results)
             {
-              result_dict = results[i]
+              result_dict = results[i];
               if(result_dict['name'] == 'psipass2' )
               {
                 var match = regex.exec(result_dict['result_data'])
                 if(match)
                 {
-                  get_file(result_dict['result_data'])
-                  ractive.set("waiting", data['last_message'])
+
+                  get_file(result_dict['result_data']);
+                  ractive.set("waiting", data['last_message']);
                 }
               }
             }
-            clearInterval(interval)
+            clearInterval(interval);
           }
           if(data[k] == 'Error' || data[k] == 'Crash')
           {
@@ -80,82 +82,81 @@ ractive.once('poll_trigger', function(){
           }
         }
       }
-    }, 2000)
+    }, 2000);
 
 },{init: false,
    defer: true
  }
-)
+);
 
 ractive.on('submit', function(event) {
-      seq = this.get('sequence')
-      seq = seq.replace(/^>.+$/mg, "").toUpperCase()
-      seq = seq.replace(/\n|\s/g,"")
-      name = this.get('name')
-      email = this.get('email')
-      psipred_job = this.get('psipred_job')
-      psipred_checked = this.get('psipred_checked')
+      seq = this.get('sequence');
+      seq = seq.replace(/^>.+$/mg, "").toUpperCase();
+      seq = seq.replace(/\n|\s/g,"");
+      name = this.get('name');
+      email = this.get('email');
+      psipred_job = this.get('psipred_job');
+      psipred_checked = this.get('psipred_checked');
       /*verify that everything here is ok*/
-      error_message=null
-      error_message = verify_form(seq, job_name, email, [psipred_checked])
+      error_message=null;
+      error_message = verify_form(seq, name, email, [psipred_checked]);
       if(error_message.length > 0)
-      (
-        this.set('form_error', error_message)
-      )
+      {
+        this.set('form_error', error_message);
+      }
       else {
-
         ractive.set( 'visible', null );
         ractive.set( 'visible', 2 );
-        var job_name = "nada"
+        var job_name = "nada";
         if(psipred_checked === true)
         {
-          job_name = "psipred"
+          job_name = "psipred";
         }
-
+        var file = null;
         try {
-          var file = new File([seq], 'input.txt');
+          file = new File([seq], 'input.txt');
         } catch (e) {
-          alert(e)
+          alert(e);
         }
 
         var fd = new FormData();
-        fd.append("input_data", file)
-        fd.append("job",job_name)
-        fd.append("submission_name",name)
-        fd.append("email",email)
-        fd.append("task1_all", true)
-        fd.append("task2_number", 12)
+        fd.append("input_data", file);
+        fd.append("job",job_name);
+        fd.append("submission_name",name);
+        fd.append("email",email);
+        fd.append("task1_all", true);
+        fd.append("task2_number", 12);
 
-        var response = send_request(submit_url, "POST", fd)
-        data = JSON.parse(response)
+        var response = send_request(submit_url, "POST", fd);
+        data = JSON.parse(response);
         for(var k in data){
           if(k == "UUID"){
-            this.set('uuid', data[k])
-            ractive.fire('poll_trigger')
+            this.set('uuid', data[k]);
+            ractive.fire('poll_trigger');
           }
         }
       }
-    event.original.preventDefault()
-})
+    event.original.preventDefault();
+});
 
 function get_file(url)
 {
-  var response = ''
+  var response = '';
   $.ajax({
     type: 'GET',
     async:   true,
     url: url,
     complete : function (file)
     {
-      ractive.set('waiting', file.responseText)
+      ractive.set('waiting', file.responseText);
     },
-    error: function (error) {alert(JSON.stringify(error))}
-  })
+    error: function (error) {alert(JSON.stringify(error));}
+  });
 }
 
 function send_request(url, type, send_data)
 {
-  var response = ''
+  var response = '';
   $.ajax({
     type: type,
     data: send_data,
@@ -168,49 +169,49 @@ function send_request(url, type, send_data)
     url: url,
     complete : function (data)
     {
-      if(data == null){alert("Failed to send data")}
+      if(data === null){alert("Failed to send data");}
       response=data.responseText;
       //alert(JSON.stringify(response, null, 2))
     },
-    error: function (error) {alert(JSON.stringify(error))}
+    error: function (error) {alert(JSON.stringify(error));}
   }).responseJSON
   return(response);
 }
 
 function verify_form(seq, job_name, email, checked_array )
 {
-  error_message = ""
+  error_message = "";
   if(! /^[\x00-\x7F]+$/.test(job_name))
   {
-    error_message = error_message + "Please restrict Job Names to valid letters numbers and basic punctuation<br />"
+    error_message = error_message + "Please restrict Job Names to valid letters numbers and basic punctuation<br />";
   }
 
   /* length checks */
   if(seq.length > 1500)
   {
-    error_message = error_message + "Your sequence is too long to process<br />"
+    error_message = error_message + "Your sequence is too long to process<br />";
   }
   if(seq.length < 30)
   {
-    error_message = error_message + "Your sequence is too short to process<br />"
+    error_message = error_message + "Your sequence is too short to process<br />";
   }
 
   /* nucleotide checks */
-  nucleotide_count = (seq.match(/A|T|C|G|U|N|a|t|c|g|u|n/g)||[]).length
+  nucleotide_count = (seq.match(/A|T|C|G|U|N|a|t|c|g|u|n/g)||[]).length;
   if((nucleotide_count/seq.length) > 0.95)
   {
-    error_message = error_message + "Your sequence appears to be nucleotide sequence. This service requires protein sequence as input<br />"
+    error_message = error_message + "Your sequence appears to be nucleotide sequence. This service requires protein sequence as input<br />";
   }
   if(/[^ACDEFGHIKLMNPQRSTVWYX_-]+/i.test(seq))
   {
-    error_message = error_message + "Your sequence contains invalid characters<br />"
+    error_message = error_message + "Your sequence contains invalid characters<br />";
   }
 
   if(isInArray(true, checked_array) === false) {
-    error_message = error_message + "You must select at least one analysis program"
+    error_message = error_message + "You must select at least one analysis program";
   }
-  return(error_message)
-};
+  return(error_message);
+}
 
 function isInArray(value, array) {
   if(array.indexOf(value) > -1)
@@ -220,8 +221,7 @@ function isInArray(value, array) {
   else {
     return(false);
   }
-};
-
+}
 /* ractive.on({
     process_form: function ( event, which ) {
       ractive.set( 'visible', null ).then( function () {
@@ -231,7 +231,3 @@ function isInArray(value, array) {
     }
   });
 */
-
-    Status API Training Shop Blog About Pricing
-
-    © 2016 GitHub, Inc. Terms Privacy Security Contact Help
