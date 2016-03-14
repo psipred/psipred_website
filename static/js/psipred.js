1cd2b1a45dc5bf08867c639b0391b4e684ac7584
Daniel Buchan <clone@myself.com>
2016-03-14 12:30:21 +0000

    correcting staging URL

--- static/js/psipred.js
+++ static/js/psipred.js
@@ -55,6 +55,9 @@ var ractive = new Ractive({
         }
 });
 
+if(location.hostname === "bioinfstage1.cs.ucl.ac.uk" || location.href  === "http://bioinf.cs.ucl.ac.uk/psipred_beta/") {
+  ractive.set('psipred_waiting_icon', '<object width="140" height="140" type="image/svg+xml" data="http://bioinf.cs.ucl.ac.uk/psipred_beta/static/images/gears.svg"/>')
+}
 
 ractive.observe('sequence', function(newValue, oldValue ) {
   regex = /^>(.+?)\s/;
