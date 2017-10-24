01b183a667c56baef4b7711eed80329527b799d0
Daneil Buchan <clone@myself.com>
2017-10-24 13:11:54 +0100

    debug times issue on staging

--- static/js/psipred.js
+++ static/js/psipred.js
@@ -567,7 +567,7 @@ function send_job(job_name, seq, name, email, ractive_instance)
   if(response_data !== null)
   {
     times = send_request(times_url,'GET',{});
-    alert(JSON.stringify(times);
+    alert(JSON.stringify(times));
     if(job_name in times)
     {
       ractive_instance.set(job_name+'_time', upper_name+" jobs typically take "+times[job_name]+" seconds");
