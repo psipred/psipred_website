375c7f5518f51c9682a6eb8b30bbe9d13331b251
Daneil Buchan <clone@myself.com>
2018-01-30 16:15:55 +0000

    updated model submission to send to simple_modeller

--- model/views.py
+++ model/views.py
@@ -1,4 +1,6 @@
-import requests
+import requests as req
+import re
+import json
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -8,16 +10,27 @@ from django.conf import settings
 
 def index(request):
     aln_url = request.GET['aln']
-    r = request.post(aln_url, data={}, files={})
-    print(r)
-    # use aln to get the alignments data to submit as the input
+    print(aln_url)
+    # use aln to get the alignments data to submit as the inpu
+    r = req.get(aln_url, data={}, files={})
+    alignment = r.text
+
     url = 'http://127.0.0.1:8000/analytics_automated/submission.json'
     data = {'job': 'pdb_modeller',
             'submission_name': 'mod_job',
-            'email': '', }
-    r = requests.post(url, data=data, files=payload)
+            'email': 'dummy@dummy.com', }
+    payload = {'input_data': ('input.txt', alignment)}
+    r = req.post(url, data=data, files=payload)
     print(r.text)
-    data = {"UUID": r.text, }
+    obj = json.loads(r.text)
+    print(obj)
     # submit URL to aa server
     # send new ID to page and have page poll for the result
+    data = {'UUID': obj['UUID']}
     return render(request, 'model/index.html', data)
+
+
+    # pdb_re = re.compile(r"^>(\d.{5})")
+    # m = re.search(pdb_re, r.text)
+    # if m:
+    #     print(m.group(1))
