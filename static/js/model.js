96b68bc1b5b0488bfcc02df8bd2e3d08d85a84be
Daniel Buchan <clone@myself.com>
2019-02-12 10:41:06 +0000

    correct modelling URIs

--- static/js/model.js
+++ static/js/model.js
@@ -25,6 +25,15 @@ if(location.hostname === "127.0.0.1" || location.hostname === "localhost")
   gears_svg = "../static/images/gears.svg";
   file_url = main_url;
 }
+else if(window.location.href === "http://bioinf.cs.ucl.ac.uk/psipred_new/" || window.location.href.includes('psipred_new'))
+{
+  app_path = '/psipred_new';
+  endpoints_url = main_url+app_path+'/api/endpoints/';
+  submit_url = main_url+app_path+'/api/submission/';
+  times_url = main_url+app_path+'/api/jobtimes/';
+  file_url = main_url+app_path+"/api";
+  //gears_svg = "../static/images/gears.svg";
+}
 else if(location.hostname === "bioinfstage1.cs.ucl.ac.uk" || location.hostname  === "bioinf.cs.ucl.ac.uk" || location.href  === "http://bioinf.cs.ucl.ac.uk/psipred_beta/") {
   endpoints_url = main_url+app_path+'/api/endpoints/';
   submit_url = main_url+app_path+'/api/submission/';
@@ -33,7 +42,7 @@ else if(location.hostname === "bioinfstage1.cs.ucl.ac.uk" || location.hostname  
   //gears_svg = "../static/images/gears.svg";
 }
 else {
-  alert('UNSETTING ENDPOINTS WARNING, WARNING!');
+  alert('UNSETTING ENDPOINTS WARNING, WARNING! MODELLING NON FUNCTIONAL');
   endpoints_url = '';
   submit_url = '';
   times_url = '';
