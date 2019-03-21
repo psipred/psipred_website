910a610e73811a7ea463f81db1682747bf210bb5
Daniel Buchan <clone@myself.com>
2019-03-21 15:47:26 +0000

    js update

--- static/js/data_tables_helper.js
+++ static/js/data_tables_helper.js
@@ -3,8 +3,8 @@ $.fn.dataTable.ext.search.push(
     function( settings, data, dataIndex ) {
         var min = parseInt( $('#min_hs_score').val(), 10 );
         var max = parseInt( $('#max_hs_score').val(), 10 );
-        var score = parseFloat( data[2] ) || 0; // use data for the age column
-
+        var score = parseFloat( data[2] ) || 0;
+        console.log('trying to search');
         if ( ( isNaN( min ) && isNaN( max ) ) ||
              ( isNaN( min ) && score <= max ) ||
              ( min <= score   && isNaN( max ) ) ||
