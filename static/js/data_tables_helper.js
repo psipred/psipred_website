09346acef1e1b50d110c80aa2a7aa02227a57c43
Daniel Buchan <clone@myself.com>
2019-03-21 15:57:15 +0000

    js update

--- static/js/data_tables_helper.js
+++ static/js/data_tables_helper.js
@@ -2,7 +2,7 @@
 $.fn.dataTable.ext.search.push(
     function( settings, data, dataIndex ) {
         console.log(settings);
-        console.log(dataIndex);
+        //console.log(dataIndex);
         var min = parseInt( $('#min_hs_score').val(), 10 );
         var max = parseInt( $('#max_hs_score').val(), 10 );
         var score = parseFloat( data[2] ) || 0;
