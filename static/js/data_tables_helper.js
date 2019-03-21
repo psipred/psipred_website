d79e5802c4fc0ee99a5b46b84f25da09dfdb2451
Daniel Buchan <clone@myself.com>
2019-03-21 16:05:57 +0000

    js update

--- static/js/data_tables_helper.js
+++ static/js/data_tables_helper.js
@@ -1,13 +1,13 @@
 $.fn.dataTable.ext.search.push(
     function( settings, data, dataIndex ) {
-        console.log("SETTINGS ID: "+settings.sTableId);
         var min;
         var max;
         var score;
-        min = parseInt( $('#min_hs_score').val(), 10 );
-        max = parseInt( $('#max_hs_score').val(), 10 );
-        score = parseFloat( data[2] ) || 0;
-
+        if(settings.sTableId === 'hspred_table'){
+          min = parseInt( $('#min_hs_score').val(), 10 );
+          max = parseInt( $('#max_hs_score').val(), 10 );
+          score = parseFloat( data[2] ) || 0;
+        }
         if ( ( isNaN( min ) && isNaN( max ) ) ||
              ( isNaN( min ) && score <= max ) ||
              ( min <= score   && isNaN( max ) ) ||
