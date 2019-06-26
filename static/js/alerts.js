616b1f910f957f88202004dbd7d26c131f602701
Daniel Buchan <clone@myself.com>
2019-06-26 15:26:34 +0100

    added dmpfold

--- static/js/alerts.js
+++ static/js/alerts.js
@@ -15,6 +15,6 @@
   var checkBox = document.getElementById("id_dmpfold_job");
   // If the checkbox is checked, display the output text
   if (checkBox.checked == true){
-    alert("DMPFold analyses can take between 1 and 5. If you wish to run multiple analyses please submit the MetaPSICOV job as a seperate submission");
+    alert("DMPFold analyses can take between 1 and 5 hours. If you wish to run multiple analyses please submit the DMPFold job as a seperate submission");
   }
  }
