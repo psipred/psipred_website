491107affee204fc370d333c7e3921b7a2e49740
Daniel <d.buchan@gold.ac.uk>
2021-03-07 17:56:25 +0000

    Update base.py

--- psipred_website/settings/base.py
+++ psipred_website/settings/base.py
@@ -53,7 +53,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'bootstrap-admin',
+    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
