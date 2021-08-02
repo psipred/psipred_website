597a10c7bd508ad2da3f5d4d45a0b05008ece140
Daniel Buchan <clone@myself.com>
2021-08-02 15:39:41 +0100

    added the ability to add new content messages to the top of the page

--- interface/admin.py
+++ interface/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(ServerSuspension)
+admin.site.register([ServerSuspension, ServerMessage])
 
 
 class ServerSuspensionAdmin(admin.ModelAdmin):
     list_display = ('pk', 'suspend', 'message', "allowed_ip")
+
+
+class ServerMessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'display', 'message')
